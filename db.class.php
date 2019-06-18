2b57725256277db64566c6d644f4de0ddb73e92a
Jewel Rana <jewelrana.dev@gmail.com>
2019-06-18 23:00:18 +0600

    First commit

---
+++ db.class.php
@@ -0,0 +1,93 @@
+    <?php
+    class db {
+    	private $conn;
+    	private $host;
+    	private $user;
+    	private $password;
+    	private $baseName;
+    	private $port;
+    	private $Debug;
+     
+        function __construct($params=array()) {
+    		$this->conn = false;
+    		$this->host = 'localhost'; //hostname
+    		$this->user = 'root'; //username
+    		$this->password = '26236566'; //password
+    		$this->dbname = 'cichat'; //name of your database
+    		$this->port = '3306';
+    		$this->debug = true;
+    		$this->connect();
+    	}
+     
+    	function __destruct() {
+    		$this->disconnect();
+    	}
+    	
+    	function connect() {
+    		if (!$this->conn) {
+    			try {
+    				$this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname.'', $this->user, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));  
+    			}
+    			catch (Exception $e) {
+    				die('Erreur : ' . $e->getMessage());
+    			}
+     
+    			if (!$this->conn) {
+    				$this->status_fatal = true;
+    				echo 'Connection BDD failed';
+    				die();
+    			} 
+    			else {
+    				$this->status_fatal = false;
+    			}
+    		}
+     
+    		return $this->conn;
+    	}
+     
+    	function disconnect() {
+    		if ($this->conn) {
+    			$this->conn = null;
+    		}
+    	}
+    	
+    	function getOne($query) {
+    		$result = $this->conn->prepare($query);
+    		$ret = $result->execute();
+    		if (!$ret) {
+    		   echo 'PDO::errorInfo():';
+    		   echo '<br />';
+    		   echo 'error SQL: '.$query;
+    		   die();
+    		}
+    		$result->setFetchMode(PDO::FETCH_ASSOC);
+    		$reponse = $result->fetch();
+    		
+    		return $reponse;
+    	}
+    	
+    	function getAll($query) {
+    		$result = $this->conn->prepare($query);
+    		$ret = $result->execute();
+    		if (!$ret) {
+    		   echo 'PDO::errorInfo():';
+    		   echo '<br />';
+    		   echo 'error SQL: '.$query;
+    		   die();
+    		}
+    		$result->setFetchMode(PDO::FETCH_ASSOC);
+    		$reponse = $result->fetchAll();
+    		
+    		return $reponse;
+    	}
+    	
+    	function execute($query) {
+    		if (!$response = $this->conn->exec($query)) {
+    			echo 'PDO::errorInfo():';
+    		   echo '<br />';
+    		   echo 'error SQL: '.$query;
+    		   die();
+    		}
+    		return $response;
+    	}
+    }
---
+++ server.php
@@ -0,0 +1,81 @@
+<?php
+// prevent the server from timing out
+set_time_limit(0);
+
+// include the web sockets server script (the server is started at the far bottom of this file)
+require 'class.PHPWebSocket.php';
+require 'db.class.php';
+$db = new db();
+
+// when a client sends data to the server
+function wsOnMessage($clientID, $message, $messageLength, $binary) {
+	global $Server, $db;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
+
+	// check if message length is 0
+	if ($messageLength == 0) {
+		$Server->wsClose($clientID);
+		return;
+	}
+
+	// //The speaker is the only person in the room. Don't let them feel lonely.
+	// if ( sizeof($Server->wsClients) == 1 )
+	// 	$Server->wsSend($clientID, "There isn't anyone else in the room, but I'll still listen to you. --Your Trusty Server");
+	// else
+	// 	//Send the message to everyone but the person who said it
+	// 	foreach ( $Server->wsClients as $id => $client )
+	// 		if ( $id != $clientID )
+	// 			$Server->wsSend($id, "Visitor $clientID ($ip) said \"$message\"");
+
+
+
+	$time = time();
+	$query = $db->execute("INSERT INTO messages (user, msg, time)
+	VALUES ('$clientID', '$message', '$time')");
+
+	if($query){
+		$Server->wsSend($clientID, $message);
+	}
+}
+
+// when a client connects
+function wsOnOpen($clientID)
+{
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
+
+	$Server->log( "$ip ($clientID) has connected." );
+
+	//Send a join notice to everyone but the person who joined
+	foreach ( $Server->wsClients as $id => $client )
+		if ( $id != $clientID )
+			$Server->wsSend($id, "Visitor $clientID ($ip) has joined the room.");
+}
+
+function wsTestMysql($message){
+	
+}
+
+// when a client closes or lost connection
+function wsOnClose($clientID, $status) {
+	global $Server;
+	$ip = long2ip( $Server->wsClients[$clientID][6] );
+
+	$Server->log( "$ip ($clientID) has disconnected." );
+
+	//Send a user left notice to everyone in the room
+	foreach ( $Server->wsClients as $id => $client )
+		$Server->wsSend($id, "Visitor $clientID ($ip) has left the room.");
+}
+
+// start the server
+$Server = new PHPWebSocket();
+$Server->bind('message', 'wsOnMessage');
+$Server->bind('testmysql', 'wsTestMysql');
+$Server->bind('open', 'wsOnOpen');
+$Server->bind('close', 'wsOnClose');
+// for other computers to connect, you will probably need to change this to your LAN IP or external IP,
+// alternatively use: gethostbyaddr(gethostbyname($_SERVER['SERVER_NAME']))
+$Server->wsStartServer('127.0.0.1', 9300);
+
+?>
